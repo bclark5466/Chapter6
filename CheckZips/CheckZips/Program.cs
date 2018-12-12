09206ac8ce0575910a079ea0152510173d7d27ad
Brandon <bclark5466@gmail.com>
2018-12-12 12:30:29 -0600

    Updated stuff

---
+++ CheckZips/CheckZips/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CheckZips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 52802, 52803, 52804, 52805, 52806, 52807, 52808, 52809, 52810, 52811 };
+            int input;
+            int i;
+
+            do
+            {
+                bool validZip = false;
+                WriteLine("Please enter a zip code");
+                input = Convert.ToInt32(ReadLine());
+                for (i = 0; i < arr.Length; i++)
+                {
+                    if (input == arr[i])
+                    {
+                        validZip = true;
+                    }
+                }
+                if (validZip)
+                {
+                    WriteLine("Zip code {0} is within our jurisdiction", input);
+                }
+                else
+                {
+                    WriteLine("I'm sorry, that zip code is outside of our boundaries");
+                }
+                
+                
+
+            } while (input != 90);
+        }
+    }
+}
---
+++ ResortPrices/ResortPrices/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ResortPrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 200, 200, 180, 180, 160, 160, 160, 145 };
+            int input;
+            int nightPrice = 1;
+            int total;
+
+            WriteLine("Please enter the number of nights you plan on staying.");
+            input = Convert.ToInt32(ReadLine());
+
+            if (input < 8)
+            {
+                nightPrice = arr[input];
+            }
+            else if (input >= 8)
+            {
+                nightPrice = arr[8];
+            }
+
+            total = input * nightPrice;
+
+            WriteLine("You plan on staying {0} nights", input);
+            WriteLine("Your total will be {0} at {1} a night", total.ToString("C"), nightPrice.ToString("C"));
+
+        }
+    }
+}
