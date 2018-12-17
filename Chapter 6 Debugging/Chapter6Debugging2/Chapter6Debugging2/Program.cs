55c094920a2dfb90db7a7402efeda6df61fe629f
Brandon <44584588+bclark5466@users.noreply.github.com>
2018-12-17 12:25:32 -0600

    Debugging. Hopefully.

---
+++ Chapter 6 Debugging/Chapter6Debugging2/Chapter6Debugging2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Program averages four numbers
+using static System.Console;
+class DebugSix02
+{
+    static void Main()
+    {
+        int[] numbers = { 12, 15, 22, 88 };
+        int x;
+        double average;
+        double total = 0;
+        Write("\nThe numbers are...");
+        for (x = 0; x < numbers.Length; ++x)
+            Write("{0} ", numbers[x]);
+        WriteLine();
+        for (x = 0; x < numbers.Length; ++x)
+        {
+            total = total + numbers[x];
+        }
+        average = total / numbers.Length;
+        Write("The average is {0} ", average);
+    }
+}
